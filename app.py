247e7b13a936dda9cdcf597ba661e70a95fb5024
av1998av <av1998av@gmail.com>
2024-10-20 19:11:59 +0530

    add authentication

--- app.py
+++ app.py
@@ -12,11 +12,14 @@ from apscheduler.triggers.date import DateTrigger
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 from flask import request
 from models import db, MessageGroupUser, MessageGroup, Job
+from functools import wraps
+import jwt
 
 database_host: str = os.getenv('DATABASE_HOST')
 database_username: str = os.getenv('DATABASE_USERNAME')
 database_password: str = os.getenv('DATABASE_PASSWORD')
 database_name: str = os.getenv('DATABASE_NAME')
+jwt_secret: str = os.getenv('JWT_SECRET')
 database_url = "mysql+pymysql://"+database_username+":"+database_password+"@"+database_host+"/"+database_name
 test_sendgrid_template = os.getenv('TEST_TEMPLATE_ID')
 
@@ -41,13 +44,33 @@ scheduler.start()
 app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 db.init_app(app)
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization']
+        if not token:
+            return jsonify({'message' : 'Token is missing !!'}), 401
+        try:
+            data = jwt.decode(token, jwt_secret, algorithms=["HS256"])            
+        except Exception as e:            
+            return jsonify({
+                'message' : 'Token is invalid !!'
+            }), 401
+        return  f(*args, **kwargs)
+  
+    return decorated
+
 @app.route('/messageGroupUsers', methods=['GET'])
+@token_required
 def list_messagegroup_users():
     data = MessageGroupUser.query.all()
     result = [z.to_json() for z in data]
     return jsonify(result)
 
 @app.route('/jobs', methods = ['GET', 'POST', 'DELETE'])
+@token_required
 def jobs():
     if request.method == 'GET':
         result = []
